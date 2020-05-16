4cba387b74d11981de861b5240ce6366ac7c3d82
NikurashinAS <55786961+NikurashinAS@users.noreply.github.com>
2020-05-17 00:06:53 +0300

    Update Main.java

--- Tasks/lab 2/Main.java
+++ Tasks/lab 2/Main.java
@@ -1,30 +1,27 @@
 package ru.spbstu.main;
+import ru.spbstu.main.shapes.*;
 
-import ru.spbstu.main.shapes.Circle;
-import ru.spbstu.main.shapes.Shape;
-import ru.spbstu.main.shapes.Rectangle;
-import ru.spbstu.main.shapes.Triangle;
-
-import java.awt.*;
-import java.security.InvalidParameterException;
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args)
     {
         Circle circle1 = new Circle(1, 2, 6);
         Circle circle2 = new Circle(3, 4, 7);
-        Circle circle3 = new Circle(5, 6, 8);
+        Circle circle3 = new Circle(7, 8, 9);
         Circle circle4 = new Circle(7, 8, 9);
-        Triangle triangle1 = new Triangle(2, 4, 6, 6, 4, 2, 5);
-        Triangle triangle2 = new Triangle(-1, -1, 2, 3, 1, 1, 10);
-        Triangle triangle3 = new Triangle(1, 7, 2, 8, 9, 3, 51);
-        Rectangle rectangle1 = new Rectangle(1, 4, 8, 8, 12);
-        Rectangle rectangle2 = new Rectangle(8, 2, 1, 3, 3);
-        Rectangle rectangle3 = new Rectangle(7, 15, 17, 3, 3);
-        Shape[] shapes = {rectangle1, circle1, triangle1, rectangle2, circle2, triangle2, rectangle3, circle3, triangle3, circle4};
+        Triangle triangle5 = new Triangle(2, 4, 6, 6, 4, 2, 5);
+        Triangle triangle6 = new Triangle(-1, -1, 2, 3, 1, 1, 10);
+        Triangle triangle7 = new Triangle(1, 7, 2, 8, 9, 3, 51);
+        Rectangle rectangle8 = new Rectangle(1, 4, 9, 8, 12);
+        Rectangle rectangle9 = new Rectangle(8, 2, 1, 3, 3);
+        Rectangle rectangle10 = new Rectangle(7, 15, 17, 3, 3);
+        Shape[] shapes = {circle1, circle2, circle3, circle4, triangle5, triangle6, triangle7, rectangle8, rectangle9,rectangle10};
 
         Shape maxArea = findBigArea(shapes);
-        System.out.println("Big shape area: " + maxArea.getArea());
+        System.out.println("Big shape area : " + maxArea.getArea());
 
 }
         /*
@@ -40,22 +37,30 @@ public class Main {
          *    статический метод в текущем классе (Main).
          */
 
-    public static Shape findBigArea(Shape[] shapes) {
+   private static Shape findBigArea(Shape[] shapes)
+   {
+       if (shapes== null)
+       {
+           throw new IllegalArgumentException("Invalid array of shapes!");
+       }
+      Shape biggestShape = shapes[0];
+      int BigArea [] = new int[10];
+      for (Shape shape : shapes)
+       {
+           if (shape.getArea() > biggestShape.getArea())
+           {
+               biggestShape = shape;
+           }
+       }
 
-        if (shapes== null)
-        {
-            throw new IllegalArgumentException("Invalid array of shapes!");
-        }
-
-        Shape maxArea= shapes[0];
-
-        for (int i = 1; i < shapes.length; i++)
-        {
-            if (shapes[i].getArea() > maxArea.getArea())
-            {
-                maxArea = shapes[i];
-            }
-        }
-        return maxArea;
-    }
+       int y = 1;
+       for (int i = 0; i< shapes.length; i++)
+       {
+           if (shapes[i].getArea() == biggestShape.getArea())
+           {
+               System.out.println("Big shape area: " + shapes[i].getClass() + (i+1));
+           }
+       }
+       return biggestShape;
+   }
 }
